6e86d174ff91ff3c4fac9edf52cc82b95f3aecf8
Aleks Kivuls <akivuls@yahoo.com>
2016-10-29 17:26:52 -0700

    Initial generation with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name acmWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the acmWebsiteApp
+ */
+angular.module('acmWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
