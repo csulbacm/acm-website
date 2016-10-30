c7124c15589be90768e82539a7b0f2ef5d6609a7
Aleks Kivuls <akivuls@yahoo.com>
2016-10-29 18:13:39 -0700

    Modify navbar and create empty pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,10 +26,25 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
+      .when('/projects', {
+        templateUrl: 'views/projects.html',
+        //controller: 'ProjectsCtrl',
+        //controllerAs: 'projects'
+      })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
         controllerAs: 'about'
+      })
+      .when('/calendar', {
+        templateUrl: 'views/calendar.html',
+        //controller: 'CalendarCtrl',
+        //controllerAs: 'calendar'
+      })
+      .when('/documents', {
+        templateUrl: 'views/documents.html',
+        //controller: 'DocumentsCtrl',
+        //controllerAs: 'documents'
       })
       .otherwise({
         redirectTo: '/'
